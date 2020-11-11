0bc5ca9c18c1dfe62144375dc1d50a859b87e794
Jonas <frankemoelle.jonas@gmail.com>
2020-11-11 21:53:12 +0100

    deleted unnecessary files

--- matrix/vector.py
+++
@@ -1,3 +0,0 @@
-class vector(MD_Operators):
-    def __init__(self):
-        
---
+++ tests/test_matrix.py
@@ -0,0 +1,17 @@
+import pytest
+
+from matrix.matrix import * 
+
+class Test_Matrix:
+
+    @pytest.fixture
+    def dummy_matrix():
+
+
+    def test_add(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "check")
